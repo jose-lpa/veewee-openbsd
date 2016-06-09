72ec3f2f115fdbb56ebaef8d8272667590214be3
Anton Bossenbroek <anton.bossenbroek@me.com>
2016-06-09 01:44:20 -0500

    Updated to ruby 2.3 support.

--- definitions/openbsd59/ruby.sh
+++ definitions/openbsd59/ruby.sh
@@ -1,14 +1,13 @@
 . /root/.profile
 pkg_add ruby-2.3.0
 
-ln -sf /usr/local/bin/ruby19 /usr/local/bin/ruby
-ln -sf /usr/local/bin/erb19 /usr/local/bin/erb
-ln -sf /usr/local/bin/irb19 /usr/local/bin/irb
-ln -sf /usr/local/bin/rdoc19 /usr/local/bin/rdoc
-ln -sf /usr/local/bin/ri19 /usr/local/bin/ri
-ln -sf /usr/local/bin/rake19 /usr/local/bin/rake
-ln -sf /usr/local/bin/gem19 /usr/local/bin/gem
-ln -sf /usr/local/bin/testrb19 /usr/local/bin/testrb
+ln -sf /usr/local/bin/ruby23 /usr/local/bin/ruby
+ln -sf /usr/local/bin/erb23 /usr/local/bin/erb
+ln -sf /usr/local/bin/irb23 /usr/local/bin/irb
+ln -sf /usr/local/bin/rdoc23 /usr/local/bin/rdoc
+ln -sf /usr/local/bin/ri23 /usr/local/bin/ri
+ln -sf /usr/local/bin/rake23 /usr/local/bin/rake
+ln -sf /usr/local/bin/gem23 /usr/local/bin/gem
 
-pkg_add ruby-gems
-pkg_add ruby-iconv
+gem update --no-document --system
+gem install --no-document rails
