d9f9499214da475622fa68261a03b6af864a961e
Anton Bossenbroek <anton.bossenbroek@me.com>
2016-06-09 01:44:52 -0500

    Added NTPD, doas and sudo.

--- definitions/openbsd59/base.sh
+++ definitions/openbsd59/base.sh
@@ -10,7 +10,7 @@ echo " "
 echo " Installing needed packages "
 echo " "
 export PKG_PATH=http://ftp3.usa.openbsd.org/pub/OpenBSD/`uname -r`/packages/`arch -s`/
-pkg_add wget curl bash vim--no_x11 rsync--iconv bzip2 ngrep
+pkg_add wget curl bash vim--no_x11 rsync--iconv bzip2 ngrep sudo-1.8.15-gettext pkglocatedb 
 
 # giving root & vagrant bash as shell
 echo " "
@@ -28,6 +28,15 @@ echo " "
 echo "# Uncomment to allow people in group wheel to run all commands without a password" >> /etc/sudoers
 echo "%wheel        ALL=(ALL) NOPASSWD: SETENV: ALL" >> /etc/sudoers
 
+# doas
+echo " "
+echo " Setting up doas to work with vagrant "
+echo " "
+echo "permit nopass keepenv { ENV PS1 SSH_AUTH_SOCK } :wheel" >> /etc/doas.conf
+echo "permit nopass keepenv root as root" >> /etc/doas.conf
+chown root:root /etc/doas.conf
+chmod 0644 /etc/doas.conf
+
 /etc/rc.d/sendmail stop
 
 # install the ports system for who wants to use it
@@ -36,5 +45,13 @@ echo " Installing the ports system ! "
 echo " "
 cd /tmp
 wget http://ftp3.usa.openbsd.org/pub/OpenBSD/`uname -r`/ports.tar.gz
+wget http://ftp.openbsd.org/pub/OpenBSD/$(uname -r)/SHA256.sig
+signify -Cp /etc/signify/openbsd-$(uname -r | cut -c 1,3)-base.pub -x SHA256.sig ports.tar.gz
 cd /usr
-sudo tar xzf /tmp/ports.tar.gz
+doas tar xzf /tmp/ports.tar.gz
+
+# Set up ntpd
+echo " "
+echo " Set ntpd to start automatically "
+echo " "
+sed 's/^ntpd_flags=$/ntpd_flags="-s"/' /etc/rc.conf > /etc/rc.conf
