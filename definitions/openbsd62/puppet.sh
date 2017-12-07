2be0affea02dfd5775480ffaa9e9ce3011d28ed4
kmonticolo <kmonticolo@gmail.com>
2017-12-07 09:03:53 +0100

    add python2.7 symlinks

--- definitions/openbsd62/puppet.sh
+++ definitions/openbsd62/puppet.sh
@@ -1,3 +1,8 @@
 # Install Puppet
 . /root/.profile
 pkg_add -I puppet
+
+ln -sf /usr/local/bin/python2.7 /usr/local/bin/python
+ln -sf /usr/local/bin/python2.7-2to3 /usr/local/bin/2to3
+ln -sf /usr/local/bin/python2.7-config /usr/local/bin/python-config
+ln -sf /usr/local/bin/pydoc2.7  /usr/local/bin/pydoc
