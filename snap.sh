87336c6f701d32da89804c1073f72504b98f20c4
kmonticolo <kmonticolo@gmail.com>
2017-12-08 14:23:03 +0100

    remove conditional

--- snap.sh
+++ snap.sh
@@ -7,10 +7,6 @@ cp build_v62 build_$ver
 sed -i ''s/openbsd62/$ver/g'' build_$ver
 cp -pR definitions/openbsd62 definitions/"$ver"
 url=$(curl -s  https://ftp.icm.edu.pl/pub/OpenBSD/"$ver"/amd64/SHA256|grep install.*iso)
-if ( [[ -z $url ]] ); then {
-cant get SHA256 fingerprint, exiting
-exit 2
-}
 sha=$(echo $url |cut -f4 -d' ')
 iso=$(echo $url |cut -f2 -d' ' | tr -d \(,\))
 
